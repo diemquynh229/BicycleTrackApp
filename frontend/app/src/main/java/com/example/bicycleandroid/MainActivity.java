9764148cecd830fad39fe3816e2198a390f52565
QuynhDuong <quynhduong@la1113.lib.iastate.edu>
2022-03-11 23:36:27 -0600

    commit

--- frontend/app/src/main/java/com/example/bicycleandroid/MainActivity.java
+++ frontend/app/src/main/java/com/example/bicycleandroid/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.bicycleandroid;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.ebanx.swipebtn.OnStateChangeListener;
@@ -12,18 +14,30 @@ import com.ebanx.swipebtn.SwipeButton;
 
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.online);
 
         SwipeButton swipeButton = findViewById(R.id.swipebutton);
         swipeButton.setOnStateChangeListener(new OnStateChangeListener() {
             @Override
             public void onStateChange(boolean active) {
+
             }
         });
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, MainActivity2.class));
+            }
+        });
+
+
 
     }
 }
